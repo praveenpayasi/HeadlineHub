fcb5debb899d043e6bba2418f254227a9b58cbf4
Usama <osamasayed585@gmai.com>
2024-04-12 19:32:47 +0200

    The GlobalTopBar composable function creates a top app bar with a title and an optional back icon. It takes parameters for the title, whether to show the back icon, and a callback for the back icon click.

--- app/src/main/java/com/praveenpayasi/headlinehub/ui/MainActivity.kt
+++ app/src/main/java/com/praveenpayasi/headlinehub/ui/MainActivity.kt
@@ -13,42 +13,43 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.core.content.ContextCompat
+import androidx.navigation.compose.rememberNavController
+import com.praveenpayasi.headlinehub.ui.base.GlobalTopBar
 import com.praveenpayasi.headlinehub.ui.base.NewsNavHost
+import com.praveenpayasi.headlinehub.ui.base.rememberHeadlineHubAppState
 import com.praveenpayasi.headlinehub.ui.theme.NewsAppTheme
 import com.praveenpayasi.headlinehub.ui.theme.gray40
-import com.praveenpayasi.headlinehub.ui.utils.AppConstant
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         askNotificationPermission()
         setContent {
+            val navController = rememberNavController()
+            val appState = rememberHeadlineHubAppState(navController)
+
             NewsAppTheme {
-                Scaffold(topBar = {
-                    TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        titleContentColor = Color.White
-                    ), title = { Text(text = AppConstant.APP_NAME) })
-                }) { padding ->
+                Scaffold(
+                    topBar = {
+                        GlobalTopBar(
+                            title = appState.showScreenTitle,
+                            shouldShowBackIcon = appState.shouldShowBackIcon,
+                            onBackClick = navController::popBackStack
+                        )
+                    }
+                ) { padding ->
                     Column(
                         modifier = Modifier
                             .padding(padding)
                             .background(gray40),
                     ) {
-                        NewsNavHost()
+                        NewsNavHost(navController)
                     }
                 }
 
